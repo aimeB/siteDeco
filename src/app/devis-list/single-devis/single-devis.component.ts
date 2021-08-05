e031e9e22b845a94b29231828ab350ecd5499826
nyamabo aimé <nyamabo.kal@gmail.com>
2021-08-05 14:33:04 +0200

    creation du projet et des components

---
+++ src/app/devis-list/single-devis/single-devis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-devis',
+  templateUrl: './single-devis.component.html',
+  styleUrls: ['./single-devis.component.scss']
+})
+export class SingleDevisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
